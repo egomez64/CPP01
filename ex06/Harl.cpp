251cd4dfa76d1e659ea51f7151b28c85c67885f4
Enzo Gomez <egomez@2F1.42angouleme.fr>
2024-11-01 14:53:42 +0100

    CPP01 fnish

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -1,24 +1,28 @@
 #include "Harl.hpp"
-#include <map>
+
+#define _DEBUG 'D' + 'E' + 'B' + 'U' + 'G'
+#define _INFO 'I' + 'N' + 'F' + 'O'
+#define _WARNING 'W' + 'A' + 'R' + 'N' + 'I' + 'N' + 'G'
+#define _ERROR 'E' + 'R' + 'R' + 'O' + 'R'
 
 void Harl::debug(void)
 {
-	std::cout <<"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "\033[32m" << "[ DEBUG ]" << std::endl << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << "\033[0m" << std::endl << std::endl;
 }
 
 void Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "\033[33m" << "[ INFO ]" << std::endl << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << "\033[0m" << std::endl << std::endl;
 }
 
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "\033[34m" << "[ WARNING ]" << std::endl << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << "\033[0m" << std::endl << std::endl;
 }
 
 void Harl::error(void)
 {
-	std::cout <<  "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "\033[35m" << "[ ERROR ]" << std::endl <<  "This is unacceptable! I want to speak to the manager now." << "\033[0m" << std::endl << std::endl;
 }
 
 void Harl::complain(const std::string &level)
@@ -29,13 +33,16 @@ void Harl::complain(const std::string &level)
 		which += *i;
 	switch (which)
 	{
-		case 'D' + 'E' + 'B' + 'U' + 'G':
+		case _DEBUG:
 			Harl::debug();
-		case 'I' + 'N' + 'F' + 'O':
+		case _INFO:
 			Harl::info();
-		case 'W' + 'A' + 'R' + 'N' + 'I' + 'N' + 'G':
+		case _WARNING:
 			Harl::warning();
-		case 'E' + 'R' + 'R' + 'O' + 'R':
+		case _ERROR:
 			Harl::error();
+			break;
+		default:
+			std::cerr << "\033[31m" << "[ Probably complaining about insignificant problems ]" << "\033[0m" << std::endl << std::endl;
 	}
 }
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -3,7 +3,10 @@
 int main(int ac, char **av)
 {
 	if (ac != 2)
+	{
+		std::cerr << "\033[31m" << "[ Too few or too many arguments ]" << "\033[0m" << std::endl << std::endl;
 		return 1;
+	}
 	Harl::complain(av[1]);
 	return 0;
 }
